c23aafdbc798f9e94fbc2b83b3e267625adfe3ed
ruchikaroy <contactruchikaroy@gmail.com>
2024-05-28 10:52:46 +0100

    This commit will complete task 1-Project Setup.

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,18 @@
-const { Pool } = require('pg');
-const ENV = process.env.NODE_ENV || 'development';
+const { Pool } = require("pg");
+const ENV = process.env.NODE_ENV || "development";
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
+const pathToEnvFile = `${__dirname}/../.env.${ENV}`;
+
+require("dotenv").config({ path: pathToEnvFile });
+
+const PGDATABASE = process.env.PGDATABASE;
+
+console.log(`The node enviroment is ${ENV}`);
+console.log(`The path is to ${pathToEnvFile}`);
+console.log(`The database is ${PGDATABASE}`);
 
 if (!process.env.PGDATABASE) {
-  throw new Error('PGDATABASE not set');
+  throw new Error("PGDATABASE not set");
 }
 
 module.exports = new Pool();
