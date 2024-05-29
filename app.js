2c368f6fefd0784ca40e1d737f2a6963e1e38578
ruchikaroy <contactruchikaroy@gmail.com>
2024-05-29 10:57:14 +0100

    This commit will complete task 5 and all its relevant pass tests.

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const { getTopics } = require("./controllers/topics.controller");
-const { getArticlesById } = require("./controllers/articles.controller");
+const {
+  getArticlesById,
+  getAllArticles,
+} = require("./controllers/articles.controller");
 const { getAPI } = require("./controllers/get-api.controller");
 
 const app = express();
 
 app.get("/api/topics", getTopics);
 app.get("/api/articles/:article_id", getArticlesById);
+app.get("/api/articles", getAllArticles);
 app.get("/api", getAPI);
 
 app.use((err, req, res, next) => {
--- models/articles.model.js
+++ models/articles.model.js
@@ -23,3 +23,21 @@ exports.fetchArticlesById = (id) => {
     })
     .catch();
 };
+exports.fetchAllArticles = () => {
+  sqlQuery = ` SELECT 
+  articles.article_id,
+  articles.title,topic,
+  articles.author,
+  articles.created_at,
+  articles.votes,
+  articles.article_img_url, 
+  CAST (COUNT (comments.article_id) AS INT) AS comment_count
+  FROM articles
+  LEFT JOIN comments ON comments.article_id = articles.article_id
+  GROUP BY articles.article_id
+  ORDER BY articles.created_at DESC`;
+
+  return db.query(sqlQuery).then(({ rows }) => {
+    return rows;
+  });
+};
