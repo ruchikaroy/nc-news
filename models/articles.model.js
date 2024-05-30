a88c3b47b31812d9b15f73ada3f676dee457b4e3
ruchikaroy <contactruchikaroy@gmail.com>
2024-05-30 11:46:13 +0100

    This commit will complete task 11 and all its relevant pass tests.

--- models/articles.model.js
+++ models/articles.model.js
@@ -23,24 +23,35 @@ exports.fetchArticlesById = (id) => {
     })
     .catch();
 };
-exports.fetchAllArticles = () => {
-  sqlQuery = ` SELECT 
+exports.fetchAllArticles = (topic) => {
+  let queryValues = [];
+
+  let sqlQuery = ` SELECT
   articles.article_id,
   articles.title,topic,
   articles.author,
   articles.created_at,
   articles.votes,
-  articles.article_img_url, 
+  articles.article_img_url,
   CAST (COUNT (comments.article_id) AS INT) AS comment_count
   FROM articles
   LEFT JOIN comments ON comments.article_id = articles.article_id
-  GROUP BY articles.article_id
+  `;
+
+  if (topic) {
+    sqlQuery += ` WHERE articles.topic = $1`;
+    queryValues.push(topic);
+  }
+  console.log(queryValues);
+  sqlQuery += ` GROUP BY articles.article_id
   ORDER BY articles.created_at DESC`;
 
-  return db.query(sqlQuery).then(({ rows }) => {
+  return db.query(sqlQuery, queryValues).then(({ rows }) => {
+    console.log(rows);
     return rows;
   });
 };
+
 exports.checkArticleExists = (article_id) => {
   return db
     .query("SELECT * FROM articles WHERE article_id = $1", [article_id])
