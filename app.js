891600f713e682d2b37ebcee2459e0c29f99db27
ruchikaroy <contactruchikaroy@gmail.com>
2024-05-31 13:54:44 +0100

    removed extra error handling middleware.

--- app.js
+++ app.js
@@ -36,10 +36,6 @@ app.use((err, req, res, next) => {
 });
 
 app.use((err, req, res, next) => {
-  res.status(400).send({ msg: "Bad Request" });
-});
-
-app.use((err, req, res, next) => {
   if (err.code === "22P02") {
     res.status(400).send({ msg: "Invalid input" });
   } else {
