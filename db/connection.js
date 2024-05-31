70171639e559511acdc32e083eeafb7ea8fbe869
ruchikaroy <contactruchikaroy@gmail.com>
2024-05-31 14:41:43 +0100

    added config object to new pool.

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,8 @@
 const { Pool } = require("pg");
 const ENV = process.env.NODE_ENV || "development";
+const pathToEnvFile = `${__dirname}/../.env.${ENV}`;
 
+require("dotenv").config({ path: pathToEnvFile });
 const config = {};
 
 if (ENV === "production") {
@@ -8,14 +10,8 @@ if (ENV === "production") {
   config.max = 2;
 }
 
-const pathToEnvFile = `${__dirname}/../.env.${ENV}`;
-
-require("dotenv").config({ path: pathToEnvFile });
-
-const PGDATABASE = process.env.PGDATABASE;
-
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error("PGDATABASE or DATABASE_URL not set");
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
